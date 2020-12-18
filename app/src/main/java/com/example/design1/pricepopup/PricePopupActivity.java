a3c6973d97025f8bbb6ba485479756d87d989f7b
Prakash Rawat <pro8ease56@gmail.com>
2020-12-18 13:56:36 +0530

    Changed Buttons to Radio Group in PricePopupActivity : 18-12-2020

--- app/src/main/java/com/example/design1/pricepopup/PricePopupActivity.java
+++ app/src/main/java/com/example/design1/pricepopup/PricePopupActivity.java
@@ -1,10 +1,12 @@
 package com.example.design1.pricepopup;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.design1.R;
+import com.google.android.material.slider.Slider;
 
 
 public class PricePopupActivity extends AppCompatActivity {
@@ -13,5 +15,13 @@ public class PricePopupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_price_popup);
+        TextView textViewCashDown = findViewById(R.id.textViewCashDownSelectPricePopup);
+        textViewCashDown.setText(getString(R.string.cash_down_price, (int) 0));
+        TextView textViewMonthlyPayment = findViewById(R.id.textViewMonthlyPaymentSelectPricePopup);
+        textViewMonthlyPayment.setText(getString(R.string.cash_down_price, (int) 0));
+        Slider sliderCashDown = findViewById(R.id.sliderCashDownPricePopup);
+        sliderCashDown.addOnChangeListener((slider, value, fromUser) -> textViewCashDown.setText(getString(R.string.cash_down_price, (int) value)));
+        Slider sliderMonthlyPayment = findViewById(R.id.sliderMonthlyPaymentPricePopup);
+        sliderMonthlyPayment.addOnChangeListener((slider, value, fromUser) -> textViewMonthlyPayment.setText(getString(R.string.cash_down_price, (int) value)));
     }
 }
